d614097ce6efac81c1a78be03a9188e6a79d6259
SecondRateCoder <olusanmi.adeosun@gmail.com>
2025-05-10 03:15:26 +0000

    ...

--- obj/Debug/net8.0-windows/MapGameWinforms.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MapGameWinforms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapGameWinforms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+254ff7f55e75e986b2989931c6821255ed7fe84e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8d45ddec5e4816d5f8641d8e38f7860cbfeb25")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapGameWinforms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapGameWinforms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
