afe1228fc407ae334892577e056e958bb6c8bcf3
Samuel Adeosun <olusanmi.adeosun@gmail.com>
2025-05-14 13:19:41 +0100

    ...

--- obj/Debug/net8.0-windows/MapGameWinforms.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MapGameWinforms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapGameWinforms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+571723dd5062ba7bb12c5ad68b2d1b9855898c98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f539e4475f49479be50f78e6984969fb05520a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapGameWinforms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapGameWinforms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
